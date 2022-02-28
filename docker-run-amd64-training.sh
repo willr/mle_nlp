e5a12c3eee921b3da650f91aa2e5334481fa02ee
Will Richardson <willbillwpr@gmail.com>
2022-02-27 22:19:31 -0500

    cmd line options

--- docker-run-amd64-training.sh
+++ docker-run-amd64-training.sh
@@ -2,8 +2,8 @@
 # docker run --rm --platform amd64 -it amd64-deploy bash
 docker run \
     --rm \
-    -v data_ignore:/app/data_ignore \
+    -v "$(pwd)/data_ignore/":/app/data_ignore/: \
     -it \
-    amd64-training \
-    bash
+    amd64-training
+    # bash
 
