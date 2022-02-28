6a93c7548d69cd0756fb847c8e7de22682496c23
Will Richardson <willbillwpr@gmail.com>
2022-02-27 21:21:51 -0500

    update to training flow

--- docker-run-amd64-training.sh
+++ docker-run-amd64-training.sh
@@ -2,8 +2,8 @@
 # docker run --rm --platform amd64 -it amd64-deploy bash
 docker run \
     --rm \
-    -v ./data_ignore:/app/data_ignore \
+    -v data_ignore:/app/data_ignore \
     -it \
-    amd64-deploy \
+    amd64-training \
     bash
 
