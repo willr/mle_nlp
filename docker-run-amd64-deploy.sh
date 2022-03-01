2e5eac337ad64e1c266dca259919765bdb14f45c
Will Richardson <willbillwpr@gmail.com>
2022-03-01 04:57:24 -0500

    testing deploy

--- docker-run-amd64-deploy.sh
+++ docker-run-amd64-deploy.sh
@@ -1,4 +1,4 @@
 #! /usr/env /bin/bash
 # docker run --rm --platform amd64 -it amd64-deploy bash
-docker run --rm -p 5000:5000 -it amd64-deploy
+docker run --rm -p 5000:5000 --env ML_MODEL_PATH='./data_ignore/bilstm5' --env TOKENIZER_JSON_PATH='./data_ignore/tokenizer.bilstm5.json' -it amd64-deploy
 
--- src/gunicorn_config.py
+++ src/gunicorn_config.py
@@ -1,2 +1,3 @@
 bind = "0.0.0.0:5000"
 workers = 1
+timeout=300
